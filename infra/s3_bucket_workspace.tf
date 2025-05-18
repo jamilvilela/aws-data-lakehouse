0fcb140a0a0f05730775f55eebc1aff16b8ff6d8
jamil vilela <jamilvilela@hotmail.com>
2025-05-18 15:31:41 -0300

    implementação inicial

---
+++ infra/s3_bucket_workspace.tf
@@ -0,0 +1,41 @@
+resource "aws_s3_bucket" "workspace_bucket" {
+  bucket = var.workspace_bucket
+  force_destroy = true        
+
+  tags = {
+    Name        = var.workspace_bucket
+    Environment = "dev"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "workspace_bucket" {
+  bucket = aws_s3_bucket.workspace_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "bucket-config" {
+  bucket = aws_s3_bucket.workspace_bucket.id
+
+  rule {
+    id = "lifecycle"
+    status = "Enabled"
+
+    filter {
+      prefix = "tmp/"
+    }
+
+    transition {
+      days          = 60
+      storage_class = "STANDARD_IA"
+    }
+
+    transition {
+      days          = 90
+      storage_class = "GLACIER"
+    }
+  }
+}
